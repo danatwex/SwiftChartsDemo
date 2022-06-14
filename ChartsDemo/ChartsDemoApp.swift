ee7e55615cce363a9b1a4aef09590989b693e7e0
Daniel Marques <daniel.marques@wexinc.com>
2022-06-14 15:35:54 -0300

    Initial Commit

---
+++ ChartsDemo/ChartsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsDemoApp.swift
+//  ChartsDemo
+//
+//  Created by Daniel Marques on 14/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ChartsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
